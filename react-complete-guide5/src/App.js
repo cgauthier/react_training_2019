33146501a60bbd0cc53f651f027929907274951f
Claude Gauthier <claude_r_gauthier@hotmail.com>
2019-08-15 16:34:20 -0400

    before deep dive into components and internals of react

---
+++ react-complete-guide5/src/App.js
@@ -0,0 +1,323 @@
+// create react app scaffolding isn't working exactly as it should
+// need to turn into ES6 syntax for App.js
+// more info on create-react-app
+// https://create-react-app.dev/docs/adding-custom-environment-variables
+
+// ******************************
+// classic React Way of working - Class Based Components
+// ******************************
+
+import React, { Component } from 'react';
+// import './App.css';
+
+// CSS Modules 
+// must edit the webpack.config.js file
+// have to restart npm start to ensure this takes effects.
+// in 16.8
+/*
+{
+              test: cssRegex,
+              exclude: cssModuleRegex,
+              use: getStyleLoaders({
+                importLoaders: 1,
+                sourceMap: isEnvProduction && shouldUseSourceMap,
+                modules: true,
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              }),
+*/              
+import cls from './App.css';
+import Person from './Person/Person';
+// A React 16+ feature, ErrorBoundary
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+// higher order components wrap around other components, for example ErrorBoundary is a higher order component
+// ErrorBoundary is effective with a build, not in development mode
+// Use ErrorBoundary only in areas where you may not always be in control of code expectation
+// for example AJAX calls may fail for some reasons....
+
+class App extends Component {
+
+  // state is managed from inside a component
+  // with react 16.8 >> you can also manage state in functional component
+  // state is something that you use only when required
+  
+  state = {
+    persons: [{
+      id: "person_1",
+      name: "Claude",
+      age: 53
+    }, {
+      id: "person_2",
+      name: "Nicole",
+      age: 40
+    }, {
+      id: "person_3",
+      name: "Schnookums",
+      age: 7,
+      children: "I'm a dog!"
+    }],
+    showPersons: false
+  }
+
+  // never manipulate the special state object directly
+  // use setState()
+  switchNameHandler = (newName) => {
+    // console.log("was clicked!");
+    this.setState( {persons: [{
+        name: "Nicole",
+        age: 40
+      }, {
+        name: newName,
+        age: 53
+      }, {
+        name: "Schnookums",
+        age: 7,
+        children: "I'm a dog!"
+      }]} 
+    )
+  }
+
+  nameChangeHandler = (event, person) => {
+
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === person.id;
+    });
+
+    const personConst = {
+      ...this.state.persons[personIndex]
+    }
+
+    // alternate way
+    // const personConst = Object.assign({}, this.state.persons[personIndex]);
+
+    personConst.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = personConst;
+
+    this.setState( {persons: persons});
+  
+  }
+
+  togglePersonsHandler = (event) => {
+    let newState = {
+      ...this.state
+    }
+    newState.showPersons = !newState.showPersons;
+    this.setState(newState);
+  }
+
+  // to deal with the change event
+  // if the target of the click is of type INPUT then we must not update the state.
+  // of course, not all targets have type....
+  deletePersonHandler = (event, personIndex) => {
+    console.log(event.target.type);
+    if(event.target.type === undefined) {
+      // always update the state immutably as best practice
+      const persons = this.state.persons.slice(); // creates an implicit copy of the entire array.
+      // or const persons = [...this.state.persons]; // spread operator
+      persons.splice(personIndex, 1);
+      this.setState({persons: persons});
+    }
+  }
+
+  render() {
+
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      color: "black",
+      fontSize: "bold",
+      cursor: "pointer"
+    };
+
+    let persons = null;
+    let btnClass = '';
+    
+    // adding key allows react to compare what was with what's coming in its virtual DOM
+    // to ensure it only modifies what changed, making it more efficient
+    // key should be unique
+
+    // moved the person.id key to the ErrorBoundary, because in iterated items
+    // the key must be in the outermost element being repeated.
+    if(this.state.showPersons) {
+      persons = (
+        <div> 
+          {this.state.persons.map((person, index) => {
+            if(person.children) {
+              return <ErrorBoundary key={person.id}><Person 
+                click={(event) => this.deletePersonHandler(event, index)}
+                name={person.name} 
+                changed={(event) => this.nameChangeHandler(event, person)}
+                age={person.age}>{person.children}</Person></ErrorBoundary>
+            } else {
+              return <ErrorBoundary key={person.id}><Person 
+                click={(event) => this.deletePersonHandler(event, index)}
+                changed={(event) => this.nameChangeHandler(event, person)}
+                name={person.name} 
+                age={person.age}/></ErrorBoundary>
+            }
+          })}
+        </div>
+      );
+
+      btnClass = cls.red;
+
+
+
+    } 
+
+    const classes = [];
+
+    // switch(this.state.persons.length) {
+
+    //     case 1:
+    //       classes.push(cls.red);
+    //       classes.push(cls.bold);
+    //     break;  
+
+    //     case 2:
+    //       classes.push(cls.red);
+          
+    //     break;
+        
+    //     default:
+    //     break;
+    // }
+
+    console.log(classes.join(" "));
+
+    return (
+        <div className={cls.App}>
+          <header className="App-header">
+            <h1>Hi, I'm a React App</h1>
+            <p className={classes.join(' ')}>This is really working!</p>
+            <button 
+              className={btnClass}
+              onClick={this.togglePersonsHandler}>Toggle Persons List</button>
+            {
+              // this.state.showPersons ? persons : null
+              persons
+            }
+          </header>
+        </div>
+    );
+  
+    // better to use .bind than the arrow function to pass data using methods
+
+    // not the most efficient way to toggle display or use conditional logic
+    /* {(this.state.showPersons) ? <div>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          change={this.nameChangeHandler}
+          click={this.switchNameHandler.bind(this, "FooBar!")}/>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>I'm a dog!</Person>            
+      </div> : ""} */
+
+  }
+}
+
+export default App;
+
+// ******************************
+// End of classic React Way of working
+// ******************************
+
+
+// ******************************
+// 16.8 + using Hooks - Functional Components as opposed to class based components
+// ******************************
+
+// hooks are the new way of working with React, but the classical way is the default way
+// hooks may mutate in the future, the API is not as solid as perceived
+// it is good to know both forms.
+// import React, { useState } from 'react';
+// import './App.css';
+// import Person from './Person/Person';
+
+// const App = (props) => {
+
+//   // hook useState
+//   // returns an array of 2 elements
+//   // element1 = currentState
+//   // element2 = function to update the state to be aware and re-render the component
+
+//   // using destructuring on useState
+//   const [personsState, setPersonsState ] = useState({
+//     persons: [{
+//       name: "Claude",
+//       age: 53
+//     }, {
+//       name: "Nicole",
+//       age: 40
+//     }, {
+//       name: "Schnookums",
+//       age: 7,
+//       children: "I'm a dog!"
+//     }]
+//   }); 
+
+//   const switchNameHandler = () => {
+
+//     // beware 
+//     // setPersonsState will update the entire personsState.. and as such
+//     // you have to ensure your entire state objects are done
+//     // best to break down using multiple useState statements and create state "slices".
+
+//     setPersonsState( {persons: [{
+//         name: "Nicole", age: 40
+//       }, {
+//         name: "Claude", age: 53
+//       }, {
+//         name: "Schnookums", age: 7, children: "I'm a dog!"
+//       }]}
+//     ); 
+//   };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Hi, I'm a React App</h1>
+//         <p>This is really working!</p>
+//         <button onClick={switchNameHandler}>Switch Name</button>
+//         <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+//         <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+//         <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>I'm a dog!</Person>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// ******************************
+// End of 16.8 + using Hooks - Functional Components as opposed to class based components
+// ******************************
+
+
+// ******************************
+// Extra Notes
+// ******************************
+
+  // what JSX compiles too.
+  // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Hi, I'm a React App'));
+  // typically we should only have one root element, even though in React JS 16+.. we can be more flexible, also return a JSON
+  // best practice is one root element
+  
+
+  // components should be in their own folder
+  // capitalize the Folder name
+
+  // we created a Person component
+
+// stateful components keep state
+// stateless components have no state
+// best to create as many stateless component and use statefull components to manage them.
+// stateful components are called smart components or container components
